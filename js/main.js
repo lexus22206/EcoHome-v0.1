93d684589171b53f23e09ca19ba652a4e231d040
lexus22206 <lexusp715@gmail.com>
2023-12-12 17:21:48 +0200

    section categories

--- js/main.js
+++ js/main.js
@@ -1,120 +1,124 @@
 // pc or touch
 const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match (/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match (/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match (/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match (/OperaMini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match (/IEMobile/i);
-    },
-    any: function () {
-        return(
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-        );
-    }
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/OperaMini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
 };
 
 if (isMobile.any()) {
-    document.body.classList.add('_touch');
+  document.body.classList.add('_touch');
 
-    // footer menu
-    const menuBtns =  document.querySelectorAll('.footer__menu-title')
-  
-    menuBtns.forEach.call(menuBtns, function(menuBtns) {
-        menuBtns.addEventListener('click', function() {
-  
-        const menuList = menuBtns.parentElement.querySelector('.footer__submenu-list')
-  
-        menuBtns.classList.toggle('_active')
-        menuList.classList.toggle('_active')
-  
-        if (menuList.classList.contains('_visible')) {
-            menuList.style.maxHeight = menuList.scrollHeight + 'px'
-          } else {
-            menuList.style.maxHeight = null
-          }
-      })
+  // footer menu
+  const menuBtns = document.querySelectorAll('.footer__menu-title')
+
+  menuBtns.forEach.call(menuBtns, function (menuBtns) {
+    menuBtns.addEventListener('click', function () {
+
+      const menuList = menuBtns.parentElement.querySelector('.footer__submenu-list')
+
+      menuBtns.classList.toggle('_active')
+      menuList.classList.toggle('_active')
+
+      if (menuList.classList.contains('_visible')) {
+        menuList.style.maxHeight = menuList.scrollHeight + 'px'
+      } else {
+        menuList.style.maxHeight = null
+      }
     })
+  })
 
 } else {
-    document.body.classList.add('_pc');
+  document.body.classList.add('_pc');
 }
 
 // burger menu
 const burger = document.querySelector('.menu__burger');
-if(burger) {
-    const menu = document.querySelector('.menu__list');
-    burger.addEventListener('click', function(e) {
-        document.body.classList.toggle('_lock');
-        burger.classList.toggle('_active');
-        menu.classList.toggle('_active');
-    });
+if (burger) {
+  const menu = document.querySelector('.menu__list');
+  burger.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    burger.classList.toggle('_active');
+    menu.classList.toggle('_active');
+  });
 }
 
-// video about
-const video = document.getElementById('about-video');
-const playPauseButton = document.getElementById('play-btn');
- 
-playPauseButton.addEventListener('click', ()=>  {
-    video.style.controls = 'true';
+// video
+$(document).on('click','.btn__video',function(){
+  var video = $(this).closest(".block__video").find(".video")[0];
   if (video.paused) {
     video.play();
-    playPauseButton.style.opacity = '0';
-    // playPauseButton.textContent = 'Pause';
+    $('.btn__video').css('opacity', '0');
   } else {
     video.pause();
-    playPauseButton.style.opacity = '1';
-    // playPauseButton.textContent = 'Play';
+    $('.btn__video').css('opacity', '1');
   }
 });
- 
+
 // swiper project
 const swiper = new Swiper('.projects__slider', {
-    spaceBetween: 10,
-    slidesPerView: 2,
-    centeredSlides: true,
-    loop: true,
+  spaceBetween: 10,
+  slidesPerView: 2,
+  centeredSlides: true,
+  loop: true,
 
-    breakpoints: {
-      640: {
-        slidesPerView: "auto",
-        spaceBetween: 16,
-      },
-      1440: {
-        slidesPerView: 4,
-        spaceBetween: 24,
-      }
+  breakpoints: {
+    640: {
+      slidesPerView: "auto",
+      spaceBetween: 16,
     },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    }
+  },
 
-  const swiperIn = new Swiper('.images__slider', {
-    slideClass : 'images__slider-item',
-      slidesPerView: 1,
-      loop: true,
-  
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-  
-  });
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  
+});
+
+const swiperIn = new Swiper('.images__slider', {
+  slideClass: 'images__slider-item',
+  slidesPerView: 1,
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+});
+
+// categories tabs
+$('.categories__item').on('click',function(){
+  var currTab = $(this).index();
+
+  $('.categories__item').removeClass('active');
+  $(this).addClass('active');
+
+  $('.categories__content').removeClass('active');
+  $('.categories__content').eq(currTab).addClass('active');
+})
